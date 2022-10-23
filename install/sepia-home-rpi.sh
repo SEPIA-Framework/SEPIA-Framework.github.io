46e97e42207d31b7ba63caa366b2c975c615f2e0
Florian Quirin <info@bytemind.de>
2022-10-23 14:50:21 +0200

    Update sepia-home-rpi.sh

--- install/sepia-home-rpi.sh
+++ install/sepia-home-rpi.sh
@@ -16,14 +16,14 @@ echo "If you are using a fresh Raspberry Pi OS installation please follow steps 
 echo "If you know what you are doing feel free to skip some steps as needed ;-)"
 echo "More help can be found here: https://github.com/SEPIA-Framework/sepia-docs/wiki"
 echo ""
-echo "Tested with: Raspberry Pi OS 'bullseye' 32/64bit (2021-12-31)"
+echo "Tested on: Raspberry Pi OS 'bullseye' 32/64bit (2021-12-31)"
 while true; do
 	echo ""
 	echo "Please choose next step:"
 	echo "1: Install Java OpenJDK 11 globally (alternative: download SEPIA-Home first and use local Java installation)"
-	echo "2: Update server-clock for precise timers"
+	echo "2: Update server-clock for precise timers and events (enable NTP)"
 	echo "3: Download SEPIA-Home bundle version: $SEPIA_VERSION"
-	echo "4: Extract SEPIA-Home to '~/SEPIA' (currently required home folder for SEPIA)"
+	echo "4: Extract SEPIA-Home to '~/SEPIA' and install some recommended (Debian) packages"
 	echo "5: Optional: Install NGINX reverse-proxy"
 	echo "6: Start setup (~/SEPIA/setup.sh)"
 	echo ""
@@ -53,13 +53,7 @@ while true; do
 
 	elif [ $option = "3" ] 
 	then
-		# INSTALL zip, unzip, curl, procps, etc.
-		sudo apt update
-		sudo apt-get install -y zip unzip curl git procps ca-certificates
-		
 		# DOWNLOAD SEPIA Custom-Bundle
-		
-		#create tmp folder (usually done before getting this file)
 		mkdir -p ~/SEPIA/tmp
 		cd ~/SEPIA/tmp
 		wget "https://github.com/SEPIA-Framework/sepia-installation-and-setup/releases/latest/download/SEPIA-Home.zip"
@@ -69,6 +63,15 @@ while true; do
 
 	elif [ $option = "4" ] 
 	then
+		# INSTALL zip, unzip, curl, procps, etc.
+		if [ -x "$(command -v apt)" ]; then
+			echo "Checking packages: zip unzip curl git procps ca-certificates"
+			sudo apt update
+			sudo apt-get install -y zip unzip curl git procps ca-certificates
+		else
+			echo "Recommended packages: zip unzip curl git procps ca-certificates"
+		fi
+		
 		# EXTRACT SEPIA-Home bundle
 		cd ~/SEPIA/tmp
 		unzip SEPIA-Home.zip -d ~/SEPIA
