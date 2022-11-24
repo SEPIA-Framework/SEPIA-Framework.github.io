72c9628609c806239fc77389279249c07be5a94e
Florian Quirin <info@bytemind.de>
2022-11-24 21:16:40 +0100

    Update sepia-client-rpi.sh

--- install/sepia-client-rpi.sh
+++ install/sepia-client-rpi.sh
@@ -6,13 +6,23 @@ echo ""
 echo "This script will download the SEPIA Client installation files for Raspberry Pi OS"
 echo "to this folder: '~/install'"
 echo ""
-echo "NOTE: It is inteded to be used with a fresh and clean Raspberry Pi OS Lite but if"
+echo "NOTE: It is intended to be used with a fresh and clean Raspberry Pi OS Lite but if"
 echo "you're feeling adventurous you can try it with other (Debian based) Linux versions as well ;-)."
 echo ""
 echo "More info: https://github.com/SEPIA-Framework/sepia-installation-and-setup/"
 echo ""
 read -p "Press any key to continue (or CTRL+C to abort)."
 echo ""
+if ! [ -x "$(command -v unzip)" ]; then
+	if [ -x "$(command -v apt)" ]; then
+		echo "Installing required packages: unzip"
+		sudo apt update
+		sudo apt install -y unzip
+	else
+		echo "Package 'unzip' is required. Please install and try again."
+		exit 1
+	fi
+fi
 # Use master or dev?
 BRANCH="master"
 PACKAGE="sepia_client_raspberrypi_os.zip"
